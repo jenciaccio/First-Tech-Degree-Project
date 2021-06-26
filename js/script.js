9c7c4e362cbf246bb4af1c3959f5a793c3235ced
JCIACCIO <thedevbunny@gmail.com>
2021-06-25 20:15:21 -0700

    added starter files
    added starter files to project

---
+++ js/script.js
@@ -0,0 +1,57 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+ ***/
+let quotes = [{
+        quote: "We believed that anything that was worth doing was worth overdoing.",
+        author: "Steven Tyler",
+        year: 1990
+    },
+
+    {
+        quote: "I used to jog but the ice cubes kept falling out of my glass.",
+        author: "David Lee Roth",
+        year: 1979
+    },
+    {
+        quote: "I'm not a Woman, I'm a force of nature",
+        author: "Courtney Love",
+        year: 1998
+
+    },
+    {
+        quote: "Life sucks, but in a beautiful kind of way",
+        author: "Axle Rose",
+        year: 1998
+    }
+
+
+];
+
+
+/***
+ * `getRandomQuote` function
+ ***/
+
+
+
+/***
+ * `printQuote` function
+ ***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+ ***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
